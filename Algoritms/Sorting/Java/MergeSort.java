23e905ae32ef6a7d0a4a0db5944436cc94cc401e
Srv108 <srv.br009@gmail.com>
2024-11-12 17:49:53 +0530

    Quick Sort

--- Algoritms/Sorting/Java/MergeSort.java
+++ Algoritms/Sorting/Java/MergeSort.java
@@ -1,55 +1,55 @@
 import java.util.*;
 public class MergeSort {
     
-public static int[] mergeSortedArray(int[] arr1,int[] arr2){
-    int m = arr1.length;
-    int n = arr2.length;
-    int i = 0, j = 0, k = 0;
-    int[] answer = new int[m+n];
-    while(i < m && j < n){
-        if(arr1[i] <= arr2[j]){
-            answer[k] = arr1[i];
-            i++;
-            k++;
-        }else{
-            answer[k] = arr2[j];
-            k++;
-            j++;
+    public static int[] mergeSortedArray(int[] arr1,int[] arr2){
+        int m = arr1.length;
+        int n = arr2.length;
+        int i = 0, j = 0, k = 0;
+        int[] answer = new int[m+n];
+        while(i < m && j < n){
+            if(arr1[i] <= arr2[j]){
+                answer[k] = arr1[i];
+                i++;
+                k++;
+            }else{
+                answer[k] = arr2[j];
+                k++;
+                j++;
+            }
         }
+
+        while(i < m){
+            answer[k] = arr1[i];
+            i++;k++;
+        }
+
+        while(j < n){
+            answer[k] = arr2[j];
+            j++;k++;
+        }
+
+        return answer;
     }
 
-    while(i < m){
-        answer[k] = arr1[i];
-        i++;k++;
+    public static int[] mergeSortHelper(int[] arr,int start,int end ){
+        if(start == end){
+            int[] ans = new int[1];
+            ans[0] = arr[start];
+            return ans;
+        }
+        int mid = (start + end) / 2;
+        int[] left = mergeSortHelper(arr,start,mid);
+        int[] right = mergeSortHelper(arr,mid+1,end);
+
+        int[] result = mergeSortedArray(left,right);
+
+        return result;
     }
 
-    while(j < n){
-        answer[k] = arr2[j];
-        j++;k++;
+    public static int[] mergeSort(int[] arr){
+        int[] answer = mergeSortHelper(arr,0,arr.length-1);
+        return answer;
     }
-
-    return answer;
-}
-
-public static int[] mergeSortHelper(int[] arr,int start,int end ){
-    if(start == end){
-        int[] ans = new int[1];
-        ans[0] = arr[start];
-        return ans;
-    }
-    int mid = (start + end) / 2;
-    int[] left = mergeSortHelper(arr,start,mid);
-    int[] right = mergeSortHelper(arr,mid+1,end);
-
-    int[] result = mergeSortedArray(left,right);
-
-    return result;
-}
-
-public static int[] mergeSort(int[] arr){
-    int[] answer = mergeSortHelper(arr,0,arr.length-1);
-    return answer;
-}
 
     public static void main(String[] args){
         int arr[] = {8,7,6,5,4,3,2,0,-1,-5,-8,1};
--- Algoritms/Sorting/Java/QuickSort.java
+++ Algoritms/Sorting/Java/QuickSort.java
@@ -1,5 +1,45 @@
-package Algoritms.Sorting.Java;
-
+import java.util.*;
 public class QuickSort {
-    
+        
+    public static void partition(int[] arr,int pivot_ele){
+        int n = arr.length;
+        int i = 0,j = 0;
+        while(j < n){
+            if(arr[j] < pivot_ele){
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+
+                i++;
+            }
+            j++;
+        }
+        
+    }
+
+    public static void quickSortHelper(int[] arr,int start,int end ){
+        if(start > end){
+            return;
+        }
+        int mid = (start + end) / 2;
+        partition(arr,arr[mid]);
+        quickSortHelper(arr,start,mid-1); // left array
+        quickSortHelper(arr,mid+1,end); // right array
+    }
+
+    public static void quickSort(int[] arr){
+        quickSortHelper(arr,0,arr.length-1);
+        
+    }
+
+    public static void main(String[] args){
+        int arr[] = {4,3,2,5,8,7,6,0,10,7};
+
+        int n = arr.length;
+        quickSort(arr);
+
+        for (int i=0;i<n;i++){
+            System.out.println(arr[i]);
+        }
+    }
 }
